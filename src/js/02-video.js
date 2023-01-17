243ad97c4825b0f99e5eaea2ca9c7693d1a0c319
PavloShyshko <111237173+PavloShyshko@users.noreply.github.com>
2023-01-17 23:00:21 +0100

    finished 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          break;
+      default:
+          break;
+  }
+});
